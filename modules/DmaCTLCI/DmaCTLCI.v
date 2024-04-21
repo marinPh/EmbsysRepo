4ec3806b11269825b33b87265551606b23e25028
marinPh <marin.philippe@epfl.ch>
2024-04-21 17:48:51 +0200

    attempt to understand

--- modules/DmaCTLCI/DmaCTLCI.v
+++ modules/DmaCTLCI/DmaCTLCI.v
@@ -2,28 +2,31 @@ module ramDmaCi #(parameter[7:0] customId = 8'h00)
                 (input wire start,
                             clock,
                             reset,
+                            bus_error,
+                            slave_dataValid,
+                            bus_aquire,
                 input wire [31:0] valueA,
                                     valueB,
                 input wire [ 7 : 0 ] ciN,
                 output wire [31:0] result,
-                output wire done);
-
-
+                output wire data_valid);
                reg [31:0]delayed_valueB;
                reg [8:0]start_address_bus;
                 reg [8:0]start_address_mem;
                 reg [7:0]burst_size;
                 reg [7:0]burst_counter;
-                //control reg
                 reg [1:0]control_reg;
                 reg [1:0]status_reg;
                 reg [9:0]block_size;
                 reg started;
                 reg r_result;
                 reg r_done;
-
+                reg writing;
+                reg burst_reset;
+                reg block_reset;
                 reg data_valid;
-                reg wanted_data;
+                wire [31:0] newA;
+                reg aquired;
 
 initial begin
     start_address_bus = 0;
@@ -36,9 +39,21 @@ end
 
 
 always @(posedge clock) begin
+
+//if bus error is 1, set burst_reset to 1
+if (bus_error == 1'b1) begin
+    burst_reset <= 1;
+    status_reg = status_reg | 2;
+    r_done <= 0;
+end
+else begin
+    burst_reset <= 0;
+    r_done <= 1;
+end
+
+//if reset is 1, reset all values
 if (reset == 1'b1)
 begin
-
     start_address_bus <= 0;
     start_address_mem <= 0;
     burst_size <= 0;
@@ -46,17 +61,37 @@ begin
     control_reg <= 0;
     block_size <= 0;
     started <= 0;
+    burst_reset <= 0;
 end
 else begin
 if (start == 1'b1) begin
     started <= 1;
 end
-if (started == 1 && ciN == customId) begin
 
+if (started == 1 && ciN == customId) begin
+if(bus_aquire == 1) begin
+    aquired <= 1;
+end
+    if(status_reg == 0 && control_reg ==1) begin
+    status_reg <=1;
+    burst_reset <=0;
+    control_reg <= 0;
+    writing <= control_reg[0];
+    end
+    if(status_reg ==1)begin
+    if (burst_counter < block_size) begin
+        r_result <= valueB;
+        r_done <= 0;
+    end
+    else begin
+        burst_reset <= 1;
+    end
+
+    end
     // check if 10th to 12th bit is equal to 1
     if (valueA[12:10] == 3'b001) begin
-        //now check if 9th bit is 1 or 0
-        if (valueA[9] == 1'b1) begin
+        //now check if 9th bit is 1 or 0  and status reg is 0
+        if (valueA[9] == 1'b1 && status_reg == 0 ) begin
           //if 9th is 1 write valueB to start_address_bus
             start_address_bus <= valueA[31:10];
             r_done <= 1;
@@ -70,7 +105,7 @@ if (started == 1 && ciN == customId) begin
     //check if 10th to 12th bit is equal to 2
     else if (valueA[12:10] == 3'b010) begin
         //check if 9th bit is 1 or 0
-        if (valueA[9] == 1'b1) begin
+        if (valueA[9] == 1'b1 && status_reg == 0) begin
             //if 9th is 1 write valueB to start_address_mem
             start_address_mem <= valueA[8:10];
             r_done <= 1;
@@ -81,11 +116,10 @@ if (started == 1 && ciN == customId) begin
             r_done <= 1;
         end
     end
-
     //check if 10th to 12th bit is equal to 3
     else if (valueA[12:10] == 3'b011) begin
         //check if 9th bit is 1 or 0
-        if (valueA[9] == 1'b1) begin
+        if (valueA[9] == 1'b1 && status_reg == 0) begin
             //if 9th is 1 write valueB to block_size
             block_size <= valueA[9:0];
             r_done <= 1;
@@ -96,11 +130,10 @@ if (started == 1 && ciN == customId) begin
             r_done <= 1;
         end
     end
-
     //check if 10th to 12th bit is equal to 4
     else if (valueA[12:10] == 3'b100) begin
         //check if 9th bit is 1 or 0
-        if (valueA[9] == 1'b1) begin
+        if (valueA[9] == 1'b1 && status_reg == 0) begin
             //if 9th is 1 write valueB to burst_size
             burst_size <= valueA[7:0];
             r_done <= 1;
@@ -111,20 +144,18 @@ if (started == 1 && ciN == customId) begin
             r_done <= 1;
         end
     end
-
     //check if 10th to 12th bit is equal to 5
     else if (valueA[12:10] == 3'b101) begin
         //check if 9th bit is 1 or 0
         if (valueA[9] == 1'b1) begin
          // if 9th is 1 write valueB to control_reg
-            control_reg <= valueA[7:0];
-            r_done <= 1;
+            control_reg = valueA[7:0];
+            r_done = 1;
         end
         else begin
             //if 9th is 0 read from control_reg
-            r_result <= status_reg;
-            r_done <= 1;
-
+            r_result = status_reg;
+            r_done = 1;
         end
     end
 end
@@ -135,34 +166,34 @@ always @(negedge clock)
 begin
     delayed_valueB <= valueB;
 end
+
+//2 different valueAs 1 for reading one by one, 1 for burst reading
+//9th bit is the reg writing bit
+assign newA = (burst_counter == 0) ? valueA : (valueA[8:0] + burst_counter ) | writing << 9;
+assign data_valid = r_done;
+assign result = r_result;
+
+
 // init ramModule
   ramDmaCi #( customId ) DUT
            (.clock(clock),
             .reset(reset),
             .start(start),
-            .valueA(valueA),
+            .valueA(newA),
             .valueB(delayed_valueB),
             .ciN(ciN),
-            .result(wanted_data),
-            .done(wanted_data));
+            .result(r_result),
+            .done(r_done));
 
-  counter #(8) counterBurstCounter
-         (.reset(reset),
+  counter #(8) BurstCounter
+         (.reset(burst_reset),
           .clock(clock),
-          .enable(started),
-          .direction(control_reg[0]),
+          .enable(status_reg & aquired),
+          .direction(1),
           .counterValue(burst_counter));
 
 
-    counter #(8) counterBlockCounter
-            (.reset(reset),
-            .clock(clock),
-            .enable(started),
-            .direction(control_reg[1]),
-            .counterValue(block_size));
 
-
-// init
 
 
 endmodule
