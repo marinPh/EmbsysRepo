cfc00a5d6df03e6c39d093a6d6776187d62413a4
vciuleanu <vciuleanu@student.ethz.ch>
2024-04-22 01:26:36 +0200

    Started adding operations

--- modules/ramDmaCi/ramDmaCi.v
+++ modules/ramDmaCi/ramDmaCi.v
@@ -1,3 +1,4 @@
+// code adapted using trial and error from modules/delay/verilog/delayIse.v
 module ramDmaCi #(  parameter [7:0] customInstructionId = 8'd0 )
                 ( input wire         clock,
                                      reset,
@@ -9,9 +10,15 @@ module ramDmaCi #(  parameter [7:0] customInstructionId = 8'd0 )
                   output wire [31:0] ciResult);
 
     wire isMyCi = (ciN == customInstructionId) ? ciStart : 1'b0;
+
+    //memory and operation
+    wire [21:0] memoryOp = ciValueA[31:10];
     wire writeEnableCPU = ciValueA[9];
-    wire writeEnableA = writeEnableCPU & isMyCi;
+    wire writeEnableA = writeEnableCPU & isMyCi & (memoryOp == 22'b000);
+    wire readEnableA = ~writeEnableCPU & isMyCi & (memoryOp == 22'b000);
     wire [8:0] addressCPU = ciValueA[8:0];
+
+    //cpu interface
     wire [31:0] dataOutCPU;
 
     // instatiate the dualPortSSRAM module
@@ -29,7 +36,7 @@ module ramDmaCi #(  parameter [7:0] customInstructionId = 8'd0 )
     wire s_delayCountZero = (s_delayCountReg == 32'd0) ? 1'd1 : 1'd0;
     wire s_delayCountOne  = (s_delayCountReg == 32'd1) ? 1'd1 : 1'd0;
     wire [31:0] s_delayCountNext = (reset == 1'b1) ? 32'd0 :
-                                    (isMyCi == 1'b1 && ~writeEnableCPU) ? 32'd1: //{9'b0, addressCPU, 14'b0} :
+                                    (readEnableA) ? 32'd1: //{9'b0, addressCPU, 14'b0} :
                                     (s_delayCountZero == 1'b0) ? s_delayCountReg - 32'd1 : s_delayCountReg;
 
     assign ciResult = (s_doneReg == 1'b1) ? dataOutCPU : 32'd0;
