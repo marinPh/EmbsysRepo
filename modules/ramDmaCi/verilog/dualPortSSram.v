d8a81cb0bcabc270edfd732bceb61fe667857b44
marinPh <marin.philippe@epfl.ch>
2024-05-12 19:04:17 +0200

    init

--- modules/ramDmaCi/verilog/dualPortSSram.v
+++ modules/ramDmaCi/verilog/dualPortSSram.v
@@ -1,6 +1,6 @@
 module dualPortSSRAM #( parameter bitwidth = 8,
                         parameter nrOfEntries = 512)
-                      ( input wire                             clock,
+                      ( input wire                             clockA, clockB,
                                                                writeEnableA, writeEnableB,
                         input wire [$clog2(nrOfEntries)-1 : 0] addressA, addressB,
                         input wire [bitwidth-1 : 0]            dataInA, dataInB,
@@ -8,16 +8,17 @@ module dualPortSSRAM #( parameter bitwidth = 8,
   
   reg [bitwidth-1 : 0] memoryContent [nrOfEntries-1 : 0];
   
-  always @(posedge clock)
+  always @(posedge clockA)
     begin
       if (writeEnableA == 1'b1) memoryContent[addressA] = dataInA;
       dataOutA = memoryContent[addressA];
     end
 
-  always @(negedge clock)
+  always @(posedge clockB)
     begin
       if (writeEnableB == 1'b1) memoryContent[addressB] = dataInB;
       dataOutB = memoryContent[addressB];
     end
 
 endmodule
+
--- programms/grayscale_accelerated/src/grayscale.c
+++ programms/grayscale_accelerated/src/grayscale.c
@@ -3,6 +3,13 @@
 #include <swap.h>
 #include <vga.h>
 
+#define WRITEBIT 1<<9
+#define BUSADDRESS 1<<10
+#define MEMADDRESS 1<<11
+#define BLOCKSIZE 3<<10
+#define BURSTSIZE 4<<10
+#define CTLREG 5<<10
+
 
 int main () {
   volatile uint16_t rgb565[640*480];
@@ -11,6 +18,9 @@ int main () {
   volatile unsigned int *vga = (unsigned int *) 0X50000020;
   camParameters camParams;
   vga_clear();
+
+  int16_t buffer1[512];
+  int8_t buffer2[512];
   
   printf("Initialising camera (this takes up to 3 seconds)!\n" );
   camParams = initOv7670(VGA);
@@ -30,19 +40,15 @@ int main () {
   while(1) {
     uint32_t * gray = (uint32_t *) &grayscale[0];
     takeSingleImageBlocking((uint32_t) &rgb565[0]);
-    asm volatile ("l.nios_rrr r0,r0,%[in2],0xC"::[in2]"r"(7));
-    for (int line = 0; line < camParams.nrOfLinesPerImage; line++) {
-      for (int pixel = 0; pixel < camParams.nrOfPixelsPerLine; pixel++) {
-        uint16_t rgb = swap_u16(rgb565[line*camParams.nrOfPixelsPerLine+pixel]);
-        //uint32_t red1 = ((rgb >> 11) & 0x1F) << 3;
-        //uint32_t green1 = ((rgb >> 5) & 0x3F) << 2;
-        //uint32_t blue1 = (rgb & 0x1F) << 3;
-        //uint32_t gray_soft = ((red1*54+green1*183+blue1*19) >> 8)&0xFF;
-        uint32_t gray = 128;
-        asm volatile ("l.nios_rrr %[out1],%[in1],r0,0xD":[out1]"=r"(gray):[in1]"r"(rgb));
-        grayscale[line*camParams.nrOfPixelsPerLine+pixel] = gray;
-        //printf("R = %d, G = %d, B = %d, packed rgb = %d, GR = %d, should be %d ", red1, green1, blue1, rgb, gray, gray_soft);
-      }
+   
+
+    //initialize the buffer
+    //give ramdmacontroller the address of the buffer
+
+    asm volatile ("l.nios_rrr r0,r0,%[in2],0xC"::[in2]"r"(1<<8|7<<4));
+
+    for(int i = 1; i < 600; i++) {
+     
     }
     asm volatile ("l.nios_rrr %[out1],r0,%[in2],0xC":[out1]"=r"(cycles):[in2]"r"(1<<8|7<<4));
     asm volatile ("l.nios_rrr %[out1],%[in1],%[in2],0xC":[out1]"=r"(stall):[in1]"r"(1),[in2]"r"(1<<9));
